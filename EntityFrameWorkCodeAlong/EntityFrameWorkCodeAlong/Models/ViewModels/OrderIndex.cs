25ddda4e21b46a9acdf6c4cc3ccc970aec88fb87
John Hellman <acoppero@gmail.com>
2017-12-15 17:14:33 +0100

    Final

---
+++ EntityFrameWorkCodeAlong/EntityFrameWorkCodeAlong/Models/ViewModels/OrderIndex.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EntityFrameWorkCodeAlong.Models.ViewModels
+{
+    public class OrderIndex
+    {
+        public int OrderNr { get; set; }
+        public string CustomerName { get; set; }
+        public string MovieTitle { get; set; }
+        public string FormatName { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public DateTime ShippingDate { get { return OrderDate.AddDays(5); } }
+
+        public bool HasBeenSent { get; set; }
+    }
+}
