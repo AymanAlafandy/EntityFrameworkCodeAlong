1604324997da08bf99d31969f348e5d0a3fd021b
swapna <pasupuletisaiswapna85@gmail.com>
2017-12-15 13:02:39 +0100

    connections given in db

---
+++ EntityFrameworkCodeAlong/EntityFrameworkCodeAlong/Models/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EntityFrameWorkCodeAlong.Models
+{
+    public class Customer
+    {
+
+        public int Id { get; set; }
+        public  string Name { get; set; }
+        public int CustomerNr { get; set; }
+        public string Adress{ get; set; }
+        public string PhoneNum { get; set; }                                                                                      //here instaed of int we are using string because int cant take too long numbers and 0 in startting
+        //[Column(TypeName="datetime2")]
+        public DateTime DateOfBirth { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }                            //virtual-- we cant instatntiate,
+    }
+
+
+
+}
---
+++ EntityFrameworkCodeAlong/EntityFrameworkCodeAlong/Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EntityFrameWorkCodeAlong.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }                                                //foreign key    
+
+
+        [ForeignKey("CustomerId")]
+        public virtual Customer Customer { get; set; }
+
+        //virtual property is creating connection for this table
+
+        public virtual ICollection<MovieFormatStock> MovieFormats { get; set;}                       //creating this to create connection bet order and mfstock 
+
+    }
+}
